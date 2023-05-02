04001db04bf68ebaef57fa97f2d06eee9ba5556d
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-02 17:39:40 +0200

    EDIT implement Pagination [Bonus]

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -9,6 +9,7 @@ export default {
 
       // Array in cui verranno messi i dati pescati tramite API dal DB
       projects: [],
+      links: []
     }
   },
 
@@ -19,15 +20,20 @@ export default {
   methods: {
 
     // Metodo che esegue chiamata al Server tramite API per recuperare tutti i Progetti dal DB
-    fetchProjects() {
+    fetchProjects(pg) {
 
-      axios.get('http://127.0.0.1:8000/api/projects')
+      axios.get('http://127.0.0.1:8000/api/projects', {
+        params: {
+          page: pg
+        }
+      })
 
         .then(res => {
           // console.log(res);
 
           // Salvo nell'Array i dati dei Progetti contenuti nella risposta mandata dal Server
-          this.projects = res.data.results;
+          this.projects = res.data.results.data;
+          this.links = res.data.results.links;
         })
 
         .catch(err => {
@@ -51,6 +57,13 @@ export default {
       <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
     </div>
   </div>
+
+  <div class="container">
+    <ul class="pagination">
+      <li :class="[link.active ? 'active' : '', 'page-link']" v-for="link in links"
+        @click="fetchProjects(parseInt(link.label))" :key="link.label" v-html="link.label"></li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -58,5 +71,21 @@ export default {
   display: grid;
   gap: 10px;
   grid-template-columns: repeat(5, 1fr);
+}
+
+.container {
+  padding: 30px 0;
+
+  .pagination {
+    display: flex;
+    gap: 10px;
+    justify-content: center;
+    cursor: pointer;
+
+    li.active {
+      text-decoration: underline;
+      color: orangered;
+    }
+  }
 }
 </style>
