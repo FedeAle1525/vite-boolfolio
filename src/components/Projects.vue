259a29e08d0599bbe88df3ba8400fd20d2832f30
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-02 16:02:29 +0200

    FEAT create component Projects for Milestone 4

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,10 +1,50 @@
 <script>
+import axios from 'axios';
 
+export default {
+
+  data() {
+    return {
+
+      // Array in cui verranno messi i dati pescati tramite API dal DB
+      projects: [],
+    }
+  },
+
+  methods: {
+
+    // Metodo che esegue chiamata al Server tramite API per recuperare tutti i Progetti dal DB
+    fetchProjects() {
+
+      axios.get('http://127.0.0.1:8000/api/projects')
+
+        .then(res => {
+          // console.log(res);
+
+          // Salvo nell'Array i dati dei Progetti contenuti nella risposta mandata dal Server
+          this.projects = res.data.results;
+        })
+
+        .catch(err => {
+          console.log(err)
+        })
+
+    }
+  },
+
+  mounted() {
+
+    this.fetchProjects();
+  }
+
+}
 </script>
 
 <template>
   <div class="container">
-    Lista Progetti
+    <p v-for="project in projects" :key="project.id">
+      {{ project.id }} - {{ project.name }}
+    </p>
   </div>
 </template>
 
