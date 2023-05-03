1026dccb300515a8f8c40343db4d949784ec358e
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-03 13:11:59 +0200

    FEAT create Root Components for Routes Router

---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,29 @@
+<script>
+import Projects from './components/Projects.vue';
+
+export default {
+
+  components: {
+    Projects
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Progetti</h1>
+  </div>
+
+  <Projects />
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 20px 0;
+
+  h1 {
+    text-align: center;
+  }
+
+}
+</style>
