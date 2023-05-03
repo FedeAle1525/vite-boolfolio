8ee2db1e84db7bb5b4f4915811a19a905510c034
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-03 13:19:08 +0200

    EDIT implement Root Components

---
+++ src/pages/About.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Parlando di Me</h1>
+
+    <p>Otaku | Gamer | Wrestling Fan | J-Pop & Rock Music</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>HomePage</h1>
+
+    <p>Benvenuto sul sito del mio PortFolio</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+}
+</style>
