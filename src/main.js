1690404873c73ff0431a4f036bf342eeb69470fb
FedeAle1525 <fedeale.1525@gmail.com>
2023-05-03 13:05:43 +0200

    FEAT install, create and mount Router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// 4 - Importare il Router
+import { router } from './router'
 
-createApp(App).mount('#app')
+// 5 - Montare il Router
+createApp(App).use(router).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,13 @@
+// 1 - Importare il Router
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 2 - Creazione del Router
+const router = createRouter({
+
+  history: createWebHistory(),
+
+  routes: []
+})
+
+// 3 - Esportazione Router
+export { router }
